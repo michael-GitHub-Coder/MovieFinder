010fa6a5ad8902a06b40c21e4d99291913d0ca14
michael-GitHub-Coder <71131317+michael-GitHub-Coder@users.noreply.github.com>
2024-09-23 11:04:45 +0200

    w

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,11 +3,49 @@ import ToggleBnt from './ToggleBnt';
 import Navbar from './Navbar';
 import { Link } from 'react-router-dom';
 import 'react-slideshow-image/dist/styles.css'
-import { Slide } from 'react-slideshow-image';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Movie = () => {
   const [movies, setMovies] = useState([]);
   const [changeValue, setChangeValue] = useState('day'); 
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    autoplay: true,  
+    autoplaySpeed: 3000,
+    slidesToShow: 9,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+            },
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2,
+            },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+    ],
+};
 
   useEffect(() => {
     const fetchdataPopular = async () => {
