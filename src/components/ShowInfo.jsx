a00e929122c51fe0d806974b8361b12fc637de8e
michael-GitHub-Coder <71131317+michael-GitHub-Coder@users.noreply.github.com>
2024-09-26 20:06:38 +0200

    w

--- src/components/ShowInfo.jsx
+++ src/components/ShowInfo.jsx
@@ -39,10 +39,25 @@ const ShowInfo = () => {
                   <img src={`https://image.tmdb.org/t/p/w500${data.poster_path}`} className='h-full w-full'/>
                 </div>
                   <div className="w-5/6 px-6 rounded overflow-hidden ">
-                    <div className="flex gap-4">
-                      <p className=" font-bold text-xl text-black ">{data.title}</p>
-                      <p className="mt-1 text-gray-400">{"("+data.release_date.substring(0,4) + ")"}</p>
+                    <div className="flex gap-2 text-xl">
+                      <p className=" font-semibold  text-black ">{data.title}</p>
+                      <p className=" text-gray-400">{"("+data.release_date.substring(0,4) + ")"}</p>
                     </div>
+                    <div>
+                    <div className="flex gap-4 items-center">
+                      <div className="bg-white h-10 w-10 rounded-full mt-4 border-l-2  border-b-2 border-black text-[10px] text-center py-2.5 font-bold text-gray-400">
+                      {data.vote_average}
+                      </div>
+                      <h1 className="text-gray-600  text-[15px] mt-3">Movie<br></br> Score</h1>
+                    
+                    </div>
+                    {/* <div className="flex gap-2">
+                      <h1 className=" font-semibold  text-black ">Score :</h1>
+                        <p className="text-[15px]  text-gray-600 mt-0.5"></p>
+                    </div> */}
+                    </div>
+                    <h1 className=" font-semibold mt-4 text-black ">Overview</h1>
+                    <p className="text-[15px]  text-gray-600 mt-2">{data.overview}</p>
                       {/* <p className="mt-2 flex flex-row">
                         <FaListAlt className="bg-gray-300 rounded-full text-4xl p-1 mr-2"/>
                         <MdFavoriteBorder className="bg-gray-300 rounded-full text-4xl p-1 mr-2"/>
