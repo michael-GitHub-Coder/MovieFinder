db20603e07abb1969dd3858e2fe3ce7e2a0c287d
michael-GitHub-Coder <71131317+michael-GitHub-Coder@users.noreply.github.com>
2024-09-26 14:41:34 +0200

    w

--- src/components/ShowInfo.jsx
+++ src/components/ShowInfo.jsx
@@ -27,29 +27,32 @@ const ShowInfo = () => {
   return (
     <>
       <Navbar />
+      
       <div className="flex flex-row space-x-5 mx-64 my-24">
-          <div className="w-64 h-80 rounded overflow-hidden shadow-xl">
+          
             {
+              
               selectedData.map((data) => 
                  data.id == id ? ( 
                 <>
+                <div className=" w-64 h-80 rounded overflow-hidden shadow-xl">
                   <img src={`https://image.tmdb.org/t/p/w500${data.poster_path}`} className='h-full w-full'/>
-                  
+                </div>
+                  <div className="w-5/6 px-6 rounded overflow-hidden ">
+                      <div className=" font-bold text-xl text-black">{data.title}</div>
+                      <p> {data.release_date}</p>
+                      <p className="mt-2 flex flex-row">
+                        <FaListAlt className="bg-gray-300 rounded-full text-4xl p-1 mr-2"/>
+                        <MdFavoriteBorder className="bg-gray-300 rounded-full text-4xl p-1 mr-2"/>
+                      </p>
+                  </div>  
                 </>
                 )
                 : null
               )
             }
           </div>
-          <div className="w-5/6 px-6 rounded overflow-hidden ">
-                <div className=" font-bold text-xl">name</div>
-                <p> PG Date category duration</p>
-                <p className="mt-2 flex flex-row">
-                  <FaListAlt className="bg-gray-300 rounded-full text-4xl p-1 mr-2"/>
-                  <MdFavoriteBorder className="bg-gray-300 rounded-full text-4xl p-1 mr-2"/>
-                </p>
-            </div>   
-       </div>
+       
     </>
   )
 }
